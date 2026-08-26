e832bff5fa47a143e6471f59d4f9d7a5b2078b99
Joshua Ojoawo <joshojoawo@gmail.com>
2026-08-26 13:51:57 +0100

    chore: lint fixed again and again

--- nestrs-microservices/src/nats.rs
+++ nestrs-microservices/src/nats.rs
@@ -80,7 +80,8 @@ impl NatsTransport {
 struct WireError {
     message: String,
     #[serde(skip_serializing_if = "Option::is_none")]
-    details: Option<serde_json::Value>,
+    // Boxed to mirror `TransportError::details` (serializes identically).
+    details: Option<Box<serde_json::Value>>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
